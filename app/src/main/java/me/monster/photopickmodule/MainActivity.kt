347fe6b523a2423b0f9043547b1fe9fd48ac4e82
jiangjiwei <jiangjiwei0322@outlook.com>
2019-11-14 12:41:11 +0800

    demo 1. 新增演示

--- app/src/main/java/me/monster/photopickmodule/MainActivity.kt
+++ app/src/main/java/me/monster/photopickmodule/MainActivity.kt
@@ -1,12 +1,63 @@
 package me.monster.photopickmodule
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import me.iwf.photopicker.PhotoPicker
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_test.setOnClickListener { preCheck() }
+    }
+
+    private fun preCheck() {
+        val permissionList = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        val actualCheckPermissionList = mutableListOf<String>()
+        for (s in permissionList) {
+            if (ContextCompat.checkSelfPermission(this, s) == PackageManager.PERMISSION_DENIED) {
+                actualCheckPermissionList.add(s)
+            }
+        }
+        if (actualCheckPermissionList.isEmpty()) {
+            openSelector()
+            return
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(actualCheckPermissionList.toTypedArray(), 123)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            openSelector()
+        }
+    }
+
+    private fun openSelector() {
+        PhotoPicker.builder()
+            .setPhotoCount(3)
+            .setShowCamera(true)
+            .setShowGif(false)
+            .setPreviewEnabled(true)
+            .setGridColumnCount(3)
+            .start(this)
+//            .start(mContext, this, PhotoPicker.REQUEST_CODE)
     }
 }
